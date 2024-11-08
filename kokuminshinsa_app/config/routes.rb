9c1e3907f1313e6a463b70dfd5eb3b60833cf1a7
inasan1234 <ryota.ina.0701@gmail.com>
2024-11-08 06:02:52 +0000

    static pages

--- kokuminshinsa_app/config/routes.rb
+++ kokuminshinsa_app/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  get "/about", to: "static_pages#about"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
